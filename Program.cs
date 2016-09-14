84de374af7428e4ee9d317bb30b6801f986654a6
sareduhh <sarahe.kaminski@gmail.com>
2016-09-14 17:02:19 -0400

    second commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace LoopsSlide47Practice
     {
         static void Main(string[] args)
         {
+            // Write a console application that asks the user for an integer. 
+            //If that integer is evenly divisible by 3, then write “You Won!”
+            //If it isn’t, write “You Lost.” 
+            //Keep asking them for a number (looping)until they win.
+
+            
+
+            for (int i = 0; i>=0; i++)
+            {
+                Console.WriteLine("Enter an integer divisible by 3.");
+                int userNumber = int.Parse(Console.ReadLine());
+                if (userNumber % 3 == 0)
+                {
+                    Console.WriteLine("You Won!");
+                }
+                else
+                {
+                    Console.WriteLine("You Lost.");
+                }
+            }
+
         }
     }
 }
